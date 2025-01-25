d946faa8605dfab001e4886e57ffef66c3f883c1
nomi135 <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>
2025-01-25 15:46:23 +0500

    Add project files.

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")] // /api/users
+    [ApiController]
+    public class UsersController(DataContext context) : ControllerBase
+    {
+        [HttpGet] // /api/users
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await context.Users.ToListAsync();
+            return Ok(users);
+        }
+
+        [HttpGet("{id:int}")] // /api/users/1
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            var user = await context.Users.FindAsync(id);
+            if (user == null) return NotFound();
+
+            return Ok(user);
+        }
+    }
+}
---
+++ API/Program.cs
@@ -0,0 +1,27 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            var app = builder.Build();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+} 
