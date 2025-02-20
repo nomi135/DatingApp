7921bc286141422fc895c7aa9f0451928108b229
nomi135 <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>
2025-02-21 00:54:09 +0500

    member update and persisting changes

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -7,11 +7,12 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace API.Controllers
 {
     [Authorize]
-    public class UsersController(IUserRepository userRepository) : BaseApiController
+    public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
     {
         [HttpGet] // /api/users
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
@@ -30,6 +31,24 @@ namespace API.Controllers
 
             return user;
 
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if(username == null) return BadRequest("No username found in token");
+
+            var user = await userRepository.GetUserByUsernameAsync(username);
+            
+            if (user == null) return BadRequest("Could not find user");
+
+            mapper.Map(memberUpdateDto, user);
+
+            if (await userRepository.SaveAllAsync()) return NoContent();
+            
+            return BadRequest("Failed to update the user");
         }
     }
 }
