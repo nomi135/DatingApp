0ed942214c3a6137d1be662618916d14580c9333
nomi135 <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>
2025-05-18 19:48:22 +0500

    SignalR

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -20,9 +20,26 @@ namespace API.Data
             context.Messages.Remove(message);
         }
 
+        public void AddGroup(Group group)
+        {
+            context.Groups.Add(group);
+        }
+
+        public async Task<Connection?> GetConnection(string connectionId)
+        {
+            return await context.Connections.FindAsync(connectionId);
+        }
+
         public async Task<Message?> GetMessage(int id)
         {
             return await context.Messages.FindAsync(id);
+        }
+
+        public async Task<Group?> GetMessageGroup(string groupName)
+        {
+            return await context.Groups
+                .Include(x => x.Connections)
+                .FirstOrDefaultAsync(x => x.Name == groupName);
         }
 
         public async Task<PagedList<MessageDto>> GetMessagesForUser(MessageParams messageParams)
@@ -46,12 +63,11 @@ namespace API.Data
         public async Task<IEnumerable<MessageDto>> GetMessageThread(string currentUsername, string recipientUsername)
         {
             var messages = await context.Messages
-                .Include(x => x.Sender).ThenInclude(x => x.Photos)
-                .Include(x => x.Recipient).ThenInclude(x => x.Photos)
                 .Where(x => x.RecipientUsername == currentUsername && x.RecipientDeleted == false 
                        && x.SenderUsername == recipientUsername && x.SenderDeleted == false 
                        || x.SenderUsername == currentUsername && x.RecipientUsername == recipientUsername)
                 .OrderBy(x => x.MessageSent)
+                .ProjectTo<MessageDto>(mapper.ConfigurationProvider) 
                 .ToListAsync();
 
             var unreadMessages = messages.Where(x => x.DateRead == null && x.RecipientUsername == currentUsername).ToList();
@@ -62,12 +78,25 @@ namespace API.Data
                 await context.SaveChangesAsync();
             }
 
-            return mapper.Map<IEnumerable<MessageDto>>(messages);
+            return messages;
+        }
+
+        public void RemoveConnection(Connection connection)
+        {
+            context.Connections.Remove(connection);
         }
 
         public async Task<bool> SaveAllAsync()
         {
             return await context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<Group?> GetGroupForConnection(string connectionId)
+        {
+            return await context.Groups
+                .Include(x => x.Connections)
+                .Where(x => x.Connections.Any(c => c.ConnectionId == connectionId))
+                .FirstOrDefaultAsync();
         }
     }
 }
---
+++ API/Entities/Connection.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities
+{
+    public class Connection
+    {
+        [Key]
+        public required string ConnectionId { get;set; }
+        public required string Username { get; set; }
+    }
+}
